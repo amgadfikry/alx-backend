00d64a525e87cfdf955c0cf2c9fe278490a28397
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-12-30 19:55:42 +0200

    create function get index of start and end of each item in page

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" module that paginate simple method """
+
+
+def index_range(page, page_size):
+    """ function get index of start and end
+        of items per page
+        Params:
+            page: page number
+            page_size: number of items per page
+        Return:
+            tuple of index start and end of items
+    """
+    if page < 1:
+        return (0,0)
+    end = page * page_size
+    return (end - page_size, end)
