5ceab183bd7fa01576e6dd90c03b64c2aba27b96
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-01 19:49:31 +0200

    use index range in class as method

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+""" module that paginate simple method """
+import csv
+import math
+from typing import List
+
+
+def index_range(page, page_size):
+    """ function get index of start and end
+        of items per page
+        Params:
+            page: page number
+            page_size: number of items per page
+        Return:
+            tuple of index start and end of items
+    """
+    if page < 1:
+        return (0, 0)
+    end = page * page_size
+    return (end - page_size, end)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        """ magic mrthod to initate in instance"""
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """method get page elements"""
+        assert type(page) is int and type(page_size) is int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        return self.dataset()[start:end]
