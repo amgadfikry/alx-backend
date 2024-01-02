df5bc58a113a2d39aeaa5712ccccb2202dfd8afe
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-02 19:38:06 +0200

    add method for concept LIFO in cache system

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" module represent first in first out """
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ class represent how cash work
+        with last in first out concept
+    """
+
+    def __init__(self):
+        """ init magic method """
+        super().__init__()
+
+    def put(self, key, item):
+        """ method to add new item to dict of cache """
+        if key is None or item is None:
+            return
+        if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+            last = self.cache_data.popitem()
+            print(f'DISCARD: {last}')
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ method to get item from cache
+            params:
+                key: key of new item
+        """
+        return self.cache_data.get(key, None)
