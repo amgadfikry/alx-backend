98f692dd679ff0692bbf22736455fd934d39c6a3
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-02 20:33:39 +0200

    add method for concept LRU in cache system

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" module represent first in first out """
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ class represent how cash work
+        with least recently used concept
+    """
+
+    def __init__(self):
+        """ init magic method """
+        self.order = {}
+        super().__init__()
+
+    def put(self, key, item):
+        """ method to add new item to dict of cache """
+        if key is None or item is None:
+            return
+        if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+            min_val = min(self.order.values())
+            li_keys = [k for k, v in self.order.items() if v == min_val]
+            print(f'DISCARD: {li_keys[0]}')
+            del self.cache_data[li_keys[0]]
+            del self.order[li_keys[0]]
+        self.cache_data[key] = item
+        self.order[key] = 0
+
+    def get(self, key):
+        """ method to get item from cache
+            params:
+                key: key of new item
+        """
+        if key in self.order:
+            self.order[key] += 1
+        return self.cache_data.get(key, None)
