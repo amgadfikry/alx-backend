402e29f310833f599ced009047d6242eb885a2c7
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-02 18:42:23 +0200

    add new class to add itmes ad retrive it from cache system

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" basic cache module """
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ class for create basic cache
+        how to save item and how get in cache
+    """
+
+    def put(self, key, item):
+        """ method to add new item to cache
+            params:
+                key: key of new item
+                value: value of item
+        """
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ method to get item from cache
+            params:
+                key: key of new item
+        """
+        value = self.cache_data.get(key)
+        if value:
+            return value
+        return None
