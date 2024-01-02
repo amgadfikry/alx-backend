7803eeaead6db479e03d26c5906fb6a13c64a5bc
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-02 18:58:53 +0200

    remove typing and improve code

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """ basic cache module """
 from base_caching import BaseCaching
-from typing import Union
 
 
 class BasicCache(BaseCaching):
@@ -9,7 +8,7 @@ class BasicCache(BaseCaching):
         how to save item and how get in cache
     """
 
-    def put(self, key: Union[None, str], item: Union[str, None]) -> None:
+    def put(self, key, item):
         """ method to add new item to cache
             params:
                 key: key of new item
@@ -17,12 +16,11 @@ class BasicCache(BaseCaching):
         """
         if key is not None or item is not None:
             self.cache_data[key] = item
+        return
 
-    def get(self, key: Union[None, str]) -> Union[str, None]:
+    def get(self, key):
         """ method to get item from cache
             params:
                 key: key of new item
         """
-        if key is None or key not in self.cache_data:
-        	return None
-        return self.cache_data.get(key)
+        return self.cache_data.get(key, None)
