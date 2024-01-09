99422fddb18b92065a0bfc4e55e87f6964f2a293
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-09 20:45:41 +0200

    install flask-babel and configure it

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" module to basic setup of flask """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """ class to create configuraton languages """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def get_template():
+    """ function that render route '/' and it's template """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
