42bca0f84e81384547c491c7fdd17f806433beb5
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-09 20:51:05 +0200

    create get_locale function

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" module to basic setup of flask """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """ class to create configuraton languages """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+@babel.localeselector
+def get_locale():
+    """ determine best match with language """
+    request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def get_template():
+    """ function that render route '/' and it's template """
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
