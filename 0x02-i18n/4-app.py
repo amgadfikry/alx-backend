28158ec2f570714a2438e82f3c5de57204c830e1
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-09 22:35:08 +0200

    use lovcal as arguments in url

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+""" module to basic setup of flask """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """ class to create configuraton languages """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+def get_locale():
+    """ determine best match with language """
+    requested_locale = request.args.get('locale')
+    if requested_locale and requested_locale in app.config['LANGUAGES']:
+        return requested_locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel.init_app(app, locale_selector=get_locale)
+
+
+@app.route('/')
+def get_template():
+    """ function that render route '/' and it's template """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
