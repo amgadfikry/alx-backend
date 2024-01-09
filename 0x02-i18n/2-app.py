f378f6e11604076d8d1da1348547b3c39438ccc9
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-09 20:54:20 +0200

    add return statement

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -20,7 +20,7 @@ app.config.from_object(Config)
 @babel.localeselector
 def get_locale():
     """ determine best match with language """
-    request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
